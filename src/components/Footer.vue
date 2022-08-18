275efe1061d0bcbe3f00bd61871ac4993097ac57
t203037 <t203037@edu.kaiyodai.ac.jp>
2022-08-18 23:48:24 +0900

    変更しました

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,24 +1,1 @@
-<template>
-  <footer class="footer">
-    <div>
-      This Application is made by
-      <span class="names">○○○, ○○○ and ○○○</span>.
-    </div>
-    <small
-      >The Application is created as a part of the GeekSalon WebExpert
-      Course.</small
-    >
-  </footer>
-</template>
-
-<style scoped>
-.footer {
-  text-align: center;
-  padding: 5rem 0;
-}
-.names {
-  display: inline-flex;
-  justify-content: center;
-  gap: 1rem;
-}
-</style>
+<template>  <footer class="footer">    <div>      This Application is made by      <span class="names">○○○, ○○○ and ○○○</span>.    </div>    <small      >The Application is created as a part of the GeekSalon WebExpert      Course.</small    >  </footer></template><style scoped>.footer {  text-align: center;  padding: 5rem 0;}.names {  display: inline-flex;  justify-content: center;  gap: 1rem;}</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,1 @@
-<template>
-  <h1>Home</h1>
-  <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
-</template>
+<template>  <h1>Home</h1>  <div>ここにそれぞれのアプリに行けるリンクを設置する</div></template>
