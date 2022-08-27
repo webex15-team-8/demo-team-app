19321615f292ff2c9cbe38a33676fd0ebe004d81
mikachi20 <mKs1m400H@gmail.com>
2022-08-27 22:27:42 +0900

    検索ページのvueファイルを作成

---
+++ src/views/serchView.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>検索ページ</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.app {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.quiz-image {
+  height: 300px;
+  width: 300px;
+  object-fit: contain;
+}
+
+.container {
+  display: flex;
+  height: 2em;
+  width: 300px;
+  padding: 1em;
+  justify-content: space-around;
+}
+</style>
